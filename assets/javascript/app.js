2c3eecd4c2a0a2504ab1379d81457e94f330a2f3
amparish <alexmparish@gmail.com>
2017-05-12 16:15:00 -0500

    game start and timer working

---
+++ assets/javascript/app.js
@@ -0,0 +1,122 @@
+/*
+	variable "trivia" object (one for each question?)
+	trivia object has "question" and "answer"
+	start button that disappears on click and makes first Q appear
+	answer choices highlighted on mouse hover
+	timer linked to question, 30 sec to answer
+
+	if A correct:
+		"correct!"
+		pause for 8 sec
+		display new question
+
+	if A wrong:
+		"wrong!"
+		highlight correct answer
+		pause 8 sec
+		display new question
+
+	if timer reaches 0:
+		"time up!"
+		highlight correct answer
+		pause 8 sec
+		display new question
+
+	when all trivia looped through:
+		show number correct/incorrect answers
+		restart button
+*/
+
+var trivia = [{
+		question: "This 90s MTV show was hosted by two sock puppets, joined by their friends Chester and Precious Roy:",
+		choices: ["Beavis & Butthead", "Clone High", "Sifl & Olly", "The Brothers Grunt"],
+		answer: 2
+	},{
+		question: "What Texas band had an unlikely Billboard Modern Rock #1 hit in 1996 with their song \"Pepper\"?",
+		choices: ["Toadies", "Butthole Surfers", "Tripping Daisy", "h"],
+		answer: 1
+	},{
+		question: "Who didn't appear as themselves in an episode of The Simpsons?",
+		choices: ["Ken Griffey, Jr.", "Buzz Aldrin", "Stephen Hawking", "Michael Jackson"],
+		answer: 3
+	},{
+		question: "Question 4?",
+		choices: ["m", "n", "o", "p"],
+		answer: 1
+	},{
+		question: "Question 5?",
+		choices: ["q", "r", "s", "t"],
+		answer: 1
+	},{
+		question: "Question 6?",
+		choices: ["u", "v", "w", "x"],
+		answer: 0
+}];
+
+var rightAns = 0;
+var wrongAns = 0;
+//var playerChoices = [];
+
+function startGame(){
+	$("#button").empty();
+
+	for (var i = 0; i < trivia.length; i++) {
+		$("#question").html("<h3>" + trivia[i].question + "</h3>");
+		$("#answerA").html("<h3>" + trivia[i].choices[0] + "</h3>");
+		$("#answerB").html("<h3>" + trivia[i].choices[1] + "</h3>");
+		$("#answerC").html("<h3>" + trivia[i].choices[2] + "</h3>");
+		$("#answerD").html("<h3>" + trivia[i].choices[3] + "</h3>");
+	};
+
+	var timeRemaining = 30;
+	setTimeout(countDown,1000);
+
+	function countDown(){
+		timeRemaining--;
+		if(timeRemaining > 0){
+	    	setTimeout(countDown,1000);
+		}
+		
+		$("#timer").html("<h2>You have " + timeRemaining + " seconds remaining to answer all the questions!");
+
+		if (timeRemaining === 0){
+			$("#timer").empty();
+			$("#question").empty();
+			$("#answerA").empty();
+			$("#answerB").empty();
+			$("#answerC").empty();
+			$("#answerD").empty();
+		};
+	}
+}
+
+
+
+/*if (playerClick === answer) {
+	rightAns++;
+	console.log(rightAns);
+}
+
+if (playerClick != answer) {
+	wrongAns++;
+	$(trivia.answer).css("background-color", "red");
+	$(trivia.answer).css("colocolor", "white");
+	console.log(wrongAns);
+}*/
+
+
+/*//Show the question after start button is clicked.
+function createQuestions() {
+    timerStart();
+    //Get question
+    var question = quesAsked[askCount]['question'];
+    //assign div element to newDiv
+    var newDiv = $('<div>');
+    //Add a class to newDIv
+    newDiv.addClass('question'); //<--so i can style it in my CSS
+    //Add text to question
+    newDiv.text(question);
+    //Add question to DOM
+    $('.trivSection').append(newDiv);
+    createAnswers(); //<--your function that loops through your questions
+*/
